3bc91e3f63fe8ab855c45d72b80e9f78b47a7b5b
Andrew Hallberg <hallbergandrew@gmail.com>
2014-05-02 11:32:24 -0700

    index and about controllerwith binding-attr pics

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
   this.route('about', { path: '/aboutus'});
-  this.route('credits');
+  this.route('credits', {path: '/thanks'});
+});
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'img/hug.jpg',
+  time: function() {
+    return(new Date()).toDateString()
+  }.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+  contactName: "Hallbeezy",
+  avatar: 'img/avatar.jpg'
+
 });
